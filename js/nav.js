efb2a66efa260f731f806f77ad2b1f0fefc3aab1
Jason Gardella <jgardell@stevens.edu>
2015-05-03 16:04:13 -0400

    More work on catalog page. Links to source/download are now optionally displayed.

--- js/nav.js
+++ js/nav.js
@@ -16,6 +16,60 @@ function getGameList() {
 	return gameList;
 }
 
+function getDevsHTML(devs) {
+	var html = "";
+	for(var i = 0; i < devs.length; i++) {
+		html = html.concat("<p>" + devs[i] + "</p>");
+	}
+	return html;
+}
+
+function getLinksHTML(src, download) {
+	var html = "";
+	if(download !== "n/a") {
+		html = html.concat("<div class='col-xs-6'>" +
+			"<a href='" + download + "' target='_blank'>" +
+				"<img class='img-responsive' src='img/download.png'/>" +
+			"</a>" +
+		"</div>");
+	}
+	else {
+		html = html.concat("<div class='col-xs-6'></div>");
+	}
+	if(src !== "n/a") {
+		html = html.concat("<div class='col-xs-6'>" +
+			"<a href='" + src + "' target='_blank'>" +
+				"<img class='img-responsive' src='img/github.png'/>" +
+			"</a>" +
+		"</div>");
+	}
+	else {
+		html = html.concat("<div class='col-xs-6'></div>");
+	}
+	return html;
+}
+
+function getGameHTML(game) {
+	var html = "";
+	html = html.concat("<div class='game-cell col-md-3 thumb'>" +
+					"<div class='row'>" +
+						"<div class='col-xs-6'>" +
+							"<img class='game-img img-responsive' src='img/" + game.img + "'/>" +
+						"</div>" +
+						"<div class='col-xs-6'>" +
+							"<p class='h4'>" + game.name + "</p>" +
+							"<p><strong>Devs</strong></p>" +
+							getDevsHTML(game.devs) +
+							"<p><strong>Event</strong></p>" +
+							"<p>" + game.event + "</p>" +
+							"<div class='row'>" +
+							getLinksHTML(game.src, game.download) +
+						"</div>" +
+					"</div>" +
+				 "</div>");
+	return html;
+}
+
 $(document).ready(function() {
 
 	$(".nav-item").click(function() {
@@ -30,29 +84,7 @@ $(document).ready(function() {
 	var gameList = getGameList();
 	for(var game in gameList) {
 		console.log(gameList[game].name);
-		table.append("<div class='game-cell col-md-3 thumb'>" +
-					 	"<div class='row'>" +
-					 		"<div class='col-xs-6'>" +
-								"<img class='game-img img-responsive' src='img/" + gameList[game].img + "'/>" +
-							"</div>" +
-							"<div class='col-xs-6'>" +
-								"<p class='lead'>" + gameList[game].name + "</p>" +
-								"<p class='text-left'><strong>Devs:</strong> " + gameList[game].devs + "</p>" +
-								"<p class='text-left'><strong>Event:</strong> " + gameList[game].event + "</p>" +
-								"<div class='row'>" +
-									"<div class='col-xs-6'>" +
-										"<a href='" + gameList[game].download + "'>" +
-											"<img class='img-responsive' src='img/github.png'/>" +
-										"</a>" +
-									"</div>" +
-									"<div class='col-xs-6'>" +
-										"<a href='" + gameList[game].src + "'>" +
-											"<img class='img-responsive' src='img/download.png'/>" +
-										"</a>" +
-									"</div>" +
-							"</div>" +
-						"</div>" +
-					 "</div>");
+		table.append(getGameHTML(gameList[game]));
 	}
 
 });
