d75c415167602b3b93fc59baf83a5bb1e804f561
Jason Gardella <jgardell@stevens.edu>
2015-05-20 18:46:06 -0400

    Use Mustache for game templating.
    - As a consequence of this, json files for games now should simply not have a download or src element if they don't have a link, instead of having n/a.

--- js/nav.js
+++ js/nav.js
@@ -16,62 +16,7 @@ function getGameList() {
 	return gameList;
 }
 
-function getDevsHTML(devs) {
-	var html = "";
-	for(var i = 0; i < devs.length; i++) {
-		html = html.concat("<p>" + devs[i] + "</p>");
-	}
-	return html;
-}
-
-function getLinksHTML(src, download) {
-	var html = "";
-	if(download !== "n/a") {
-		html = html.concat("<div class='col-xs-6'>" +
-			"<a href='" + download + "' target='_blank'>" +
-				"<img class='img-responsive' src='img/download.png'/>" +
-			"</a>" +
-		"</div>");
-	}
-	else {
-		html = html.concat("<div class='col-xs-6'></div>");
-	}
-	if(src !== "n/a") {
-		html = html.concat("<div class='col-xs-6'>" +
-			"<a href='" + src + "' target='_blank'>" +
-				"<img class='img-responsive' src='img/github.png'/>" +
-			"</a>" +
-		"</div>");
-	}
-	else {
-		html = html.concat("<div class='col-xs-6'></div>");
-	}
-	return html;
-}
-
-function getGameHTML(game) {
-	var html = "";
-	html = html.concat("<div class='game-cell col-md-3 thumb'>" +
-					"<div class='row'>" +
-						"<div class='col-xs-6'>" +
-							"<img class='game-img img-responsive' src='img/" + game.img + "'/>" +
-						"</div>" +
-						"<div class='col-xs-6'>" +
-							"<p class='h4'>" + game.name + "</p>" +
-							"<p><strong>Devs</strong></p>" +
-							getDevsHTML(game.devs) +
-							"<p><strong>Event</strong></p>" +
-							"<p>" + game.event + "</p>" +
-							"<div class='row'>" +
-							getLinksHTML(game.src, game.download) +
-						"</div>" +
-					"</div>" +
-				 "</div>");
-	return html;
-}
-
 $(document).ready(function() {
-
 	$(".nav-item").click(function() {
 		// reset all nav items
 		$(".nav-item").removeClass("active");
@@ -82,9 +27,12 @@ $(document).ready(function() {
 
 	var table = $(".row");
 	var gameList = getGameList();
-	for(var game in gameList) {
-		console.log(gameList[game].name);
-		table.append(getGameHTML(gameList[game]));
-	}
+	$.get('mustache/game.mustache.html', function(template){
+		Mustache.parse(template);
+		for(var game in gameList) {
+			console.log(gameList[game].name);
+			table.append(Mustache.render(template, gameList[game]));
+		}
+	});
 
 });
