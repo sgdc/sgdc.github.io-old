dbacf353bbcdd92b9e810cded22de4fecf67fe44
jgardella <jgardell@stevens.edu>
2015-06-01 21:08:44 -0400

    Begin implementing Contact page.
    - Renamed contact page to member page.
    - Most of the functionality of the page is implemented, just needs to be made pretty.
    - The structure for members is defined in the members.json file, and each member is defined in their own json file in the members data folder.

--- js/nav.js
+++ js/nav.js
@@ -18,7 +18,54 @@ function getGameList() {
 	return gameList;
 }
 
+function getMemberList() {
+	var memberList = {
+		eboard: {},
+		minor_eboard: {},
+		general_members: []
+	};
+	$.ajaxSetup({
+		async: false
+	});
+	// get member list
+	$.getJSON("data/members.json", function(data) {
+		// get president
+		$.getJSON("data/members/" + data.eboard.president + ".json", function(member) {
+			memberList.eboard.president = member;
+		});
+		// get vice president
+		$.getJSON("data/members/" + data.eboard.vice_president + ".json", function(member) {
+			memberList.eboard.vice_president = member;
+		});
+		// get treasurer
+		$.getJSON("data/members/" + data.eboard.treasurer + ".json", function(member) {
+			memberList.eboard.treasurer = member;
+		});
+		// get secretary
+		$.getJSON("data/members/" + data.eboard.secretary + ".json", function(member) {
+			memberList.eboard.secretary = member;
+		});
+		// get alumni rep
+		$.getJSON("data/members/" + data.minor_eboard.alumni_rep + ".json", function(member) {
+			memberList.minor_eboard.alumni_rep = member;
+		});
+		// get cabinet manager
+		$.getJSON("data/members/" + data.minor_eboard.cabinet_man + ".json", function(member) {
+			memberList.minor_eboard.cabinet_man = member;
+		});
+		// get general members
+		for(var i = 0; i < data.general_members.length; i++) {
+			$.getJSON("data/members/" + data.general_members[i] + ".json", function(data) {
+				memberList.general_members.push(data);
+			});
+		}
+	});
+	return memberList;
+}
+
 $(document).ready(function() {
+
+	// nav bar functionality
 	$(".nav-item").click(function() {
 		// reset all nav items
 		$(".nav-item").removeClass("active");
@@ -27,13 +74,36 @@ $(document).ready(function() {
 		$("#myCarousel").carousel($(this).index());
 	});
 
-	var table = $(".rig");
+	// populate game catalog based on JSON data
+	var gameTable = $("#catalogSlide");
 	gameList = getGameList();
 	$.get('mustache/gamebox.mustache.html', function(template){
 		Mustache.parse(template);
 		for(var i = 0; i < gameList.length; i++) {
 			gameList[i].idx = i; // set idx of game in game list
-			table.append(Mustache.render(template, gameList[i]));
+			gameTable.append(Mustache.render(template, gameList[i]));
+		}
+	});
+
+	// populate member page based on JSON data
+	var generalMembersSection = $("#general_members");
+	var presidentSection = $("#president");
+	var vicePresidentSection = $("#vice_president");
+	var treasurerSection = $("#treasurer");
+	var secretarySection = $("#secretary");
+	var alumniRepSection = $("#alumni_rep");
+	var cabinetManagerSection = $("#cabinet_man");
+	var memberList = getMemberList();
+	$.get('mustache/member.mustache.html', function(template){
+		Mustache.parse(template);
+		presidentSection.append(Mustache.render(template, memberList.eboard.president));
+		vicePresidentSection.append(Mustache.render(template, memberList.eboard.vice_president));
+		treasurerSection.append(Mustache.render(template, memberList.eboard.treasurer));
+		secretarySection.append(Mustache.render(template, memberList.eboard.secretary));
+		alumniRepSection.append(Mustache.render(template, memberList.minor_eboard.alumni_rep));
+		cabinetManagerSection.append(Mustache.render(template, memberList.minor_eboard.cabinet_man));
+		for(var i = 0; i < memberList.general_members.length; i++) {
+			generalMembersSection.append(Mustache.render(template, memberList.general_members[i]));
 		}
 	});
 
